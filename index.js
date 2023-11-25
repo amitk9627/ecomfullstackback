69e7f60622e7f36e649f16156daf6965654f73a1
Amit kumar <ramitgzp56@gmail.com>
2023-11-26 00:23:57 +0530

    mongodb

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const port=process.env.port || 3008;
 
 const connectDB= async ()=>{
-    return mongoose.connect(process.env.mongoDburl);
+    await mongoose.connect(process.env.mongoDBurl);
 }
-connectDB().then(()=>console.log("db Connected")).catch(()=>console.log('db disconnected'));
+connectDB().then(()=>console.log("db Connected")).catch((e)=>console.log('db disconnected',e));
 
 app.listen(port,()=>{
     console.log('server listening on '+ port)
